a4a72b517bded4cfc6dbe8101e457778b96fa3a7
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-11 00:51:01 +0100

    Add linked.ts

---
+++ src/linked.test.ts
@@ -0,0 +1,42 @@
+import { LinkedList } from "./linked";
+
+describe("Linked lists", () => {
+  test("Initializes a list with undefined values", () => {
+    const list = new LinkedList<number>();
+
+    expect(list.length).toBe(0);
+    expect(list.head).toBe(undefined);
+    expect(list.tail).toBe(undefined);
+  });
+
+  test("Adds first pushed value as head and tail", () => {
+    const list = new LinkedList<string>();
+
+    list.push("degeso");
+
+    expect(list.length).toBe(1);
+    expect(list.head).toBe("degeso");
+    expect(list.tail).toBe("degeso");
+  });
+
+  test("toArray returns an array with all elements", () => {
+    const list = new LinkedList(1);
+
+    list.push(2);
+    list.push(3);
+
+    expect(list.length).toBe(3);
+    expect(list.toArray()).toEqual([1, 2, 3]);
+
+    const list2 = new LinkedList("hello");
+
+    list2.push("ciao");
+    list2.unshift("hola");
+
+    expect(list2.toArray()).toEqual(["hola", "hello", "ciao"]);
+
+    const empty = new LinkedList<number>();
+
+    expect(empty.toArray()).toEqual([]);
+  });
+});
---
+++ src/linked.ts
@@ -0,0 +1,84 @@
+class LinkedNode<T> {
+  value: T;
+  next: LinkedNode<T> | undefined;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+class LinkedList<T> {
+  private head_: LinkedNode<T> | undefined;
+  private tail_: LinkedNode<T> | undefined;
+  private length_ = 0;
+
+  constructor(value?: T) {
+    if (value !== undefined) {
+      this.unshift(value);
+    }
+  }
+
+  public unshift = (value: T) => {
+    const node = new LinkedNode(value);
+
+    if (this.head) {
+      node.next = this.head_;
+    } else {
+      this.tail_ = node;
+    }
+
+    this.head_ = node;
+    this.length_ += 1;
+  };
+
+  public push = (value: T) => {
+    const node = new LinkedNode(value);
+
+    if (this.tail_) {
+      this.tail_.next = node;
+      this.tail_ = node;
+      this.length_ += 1;
+
+      return;
+    }
+
+    this.head_ = node;
+    this.tail_ = node;
+    this.length_ += 1;
+  };
+
+  get head() {
+    if (this.head_) {
+      return this.head_.value;
+    }
+  }
+
+  get tail() {
+    if (this.tail_) {
+      return this.tail_.value;
+    }
+  }
+
+  get length() {
+    return this.length_;
+  }
+
+  public toArray = () => {
+    const generator = this.getGenerator();
+
+    return Array.from<T>(generator());
+  };
+
+  private getGenerator = () => {
+    let node = this.head_;
+
+    return function* () {
+      while (node) {
+        yield node.value;
+        node = node.next;
+      }
+    };
+  };
+}
+
+export { LinkedList };
