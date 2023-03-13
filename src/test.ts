d2f116986430d121ceff3880d349903e52657264
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-13 12:52:16 +0100

    Add minor narrowing tests

--- src/test.ts
+++ src/test.ts
@@ -7,3 +7,45 @@ export const main = () => {
 const say = (text: string) => {
   console.info(text);
 };
+
+declare const getTesting: () => {
+  foo: string[] | undefined;
+  bar: string[] | undefined;
+};
+
+const someFunction = () => {
+  const testing = getTesting();
+
+  if (!testing.foo && !testing.bar) {
+    return;
+  }
+
+  if (!testing.foo && testing.bar) {
+    return;
+  }
+
+  if (testing.foo && !testing.bar) {
+    return;
+  }
+
+  if (testing.foo && testing.bar) {
+    return;
+  }
+
+  testing.foo; // (property) foo: string[] | undefined
+  testing.bar; // (property) bar: string[] | undefined
+};
+
+const someOtherFunction = () => {
+  const testing = getTesting();
+
+  if (!testing.foo) {
+    return;
+  }
+
+  testing.foo; // (property) foo: string[]
+  testing.bar; // (property) bar: string[] | undefined
+};
+
+someFunction();
+someOtherFunction();
